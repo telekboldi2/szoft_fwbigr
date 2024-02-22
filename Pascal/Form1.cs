9c587cb011e010ac8cd5519ef578011e5384a58c
fwbigr <fwbigr@SZA0030872>
2024-02-22 12:56:25 +0100

    Add project files.

---
+++ Pascal/Form1.cs
@@ -0,0 +1,39 @@
+namespace Pascal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int m = 40; //gombméret    
+            for (int sor = 0; sor < 10; sor++)
+            {
+                for (int oszlop = 0; oszlop < sor+1; oszlop++)
+                {
+                    Button b = new Button();
+                    Controls.Add(b);
+                    b.Width = m;
+                    b.Height = m;
+                    b.Top = m*sor;
+                    b.Left = m * oszlop;
+                    int x = Factorial(sor) / (Factorial(oszlop) *(Factorial(sor-oszlop)));
+                    b.Text = x.ToString();
+                }
+            }
+        }
+
+        int Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            return n*Factorial(n-1);
+
+        }
+    }
+}
