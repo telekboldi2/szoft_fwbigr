dadc372d7e6da21d8e11a4e4ab5f2f59e072443c
Boldi <boldizsar.telek@stud.uni-corvinus.hu>
2024-02-29 12:05:51 +0100

    szorzotabla

--- VillogosGomb/Form1.cs
+++ VillogosGomb/Form1.cs
@@ -2,9 +2,29 @@ namespace VillogosGomb
 {
     public partial class Form1 : Form
     {
+        int méret = 40;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+            for (int sor = 0; sor < 10; sor++)
+            {
+                for (int oszlop = 0; oszlop < 10; oszlop++)
+                {
+                    Button b = new Button();
+                    b.Height = méret;
+                    b.Width = méret;
+                    b.Left = sor*méret;
+                    b.Top = oszlop*méret;
+                    b.Text = ((sor+1)*(oszlop+1)).ToString();
+                    Controls.Add(b);
+                }
+            }
         }
     }
 }
