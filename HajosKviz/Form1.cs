c541cb6827cee4d5d51e932f10cea7c5d3859500
Boldi <boldizsar.telek@stud.uni-corvinus.hu>
2024-03-14 12:37:21 +0100

    gyakupload1 Importing text + Válasgomb.cs

---
+++ HajosKviz/Form1.cs
@@ -0,0 +1,57 @@
+namespace HajosKviz
+{
+    public partial class Form1 : Form
+    {
+
+        List<Kérdés> ÖsszesKérdés;
+        List<Kérdés> AktuálisKérdések;
+        int AktuálisKérdés = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ÖsszesKérdés = KérdésBetöltés();
+            AktuálisKérdések = new List<Kérdés>();
+
+            for (int i = 1; i < 7; i++)
+            {
+                AktuálisKérdések.Add(ÖsszesKérdés[0]);
+                ÖsszesKérdés.RemoveAt(0);
+            }
+            dataGridView1.DataSource = AktuálisKérdések;
+        }
+
+        List<Kérdés> KérdésBetöltés()
+        {
+
+            List<Kérdés> kérdések = new List<Kérdés>();
+            StreamReader sr = new StreamReader("hajozasi_szabalyzat_kerdessor_BOM.txt", true);
+            while (!sr.EndOfStream)
+            {
+                string sor = sr.ReadLine() ?? "n/a"; // ??"n/a" = "n/a"-t ad vissza ha NULL az értéke sornak
+                string[] tömb = sor.Split("\t");
+                if (tömb.Length != 7) continue;
+                Kérdés k = new Kérdés();
+                k.KérdésSzöveg = tömb[1];
+                k.Válasz1 = tömb[2];
+                k.Válasz2 = tömb[3];
+                k.Válasz3 = tömb[4];
+                k.URL = tömb[5];
+                int x = 0;
+                int.TryParse(tömb[6], out x);
+                k.HelyesVálasz = x;
+
+                kérdések.Add(k);
+            }
+
+            sr.Close();
+
+            return kérdések;
+        }
+
+    }
+}
---
+++ HajosKviz/VálaszGomb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HajosKviz
+{
+    internal class VálaszGomb : TextBox
+    {
+        public VálaszGomb()
+        {
+            ReadOnly = true;
+            BackColor = Color.Gray;
+            BorderStyle = BorderStyle.None;
+            Multiline = true; //többsoros szöveg
+
+
+        }
+    }
+}
