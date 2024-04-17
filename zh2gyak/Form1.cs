d258b20c77f54a73ed04e17ee81b16121a28dd2f
Boldizsár Noel Telek <boldizsar.telek@stud.uni-corvinus.hu>
2024-04-17 18:33:05 +0200

    zh2gyakupdate

---
+++ zh2gyak/Form1.cs
@@ -0,0 +1,10 @@
+namespace zh2gyak
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ zh2gyak/lekepezes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zh2gyak
+{
+    public class lekepezes
+    {
+        double VersenyzoID { get; set; }
+        string Nev { get; set; } = string.Empty;
+        string Nemzetiseg { get; set; } = string.Empty;
+
+        double EredmenyPerc {  get; set; }
+        string Kategoria { get; set; } = string.Empty;
+    }
+}
